ff3d30b1e4901cc5785b5514664d944370e5f65a
leetaekyoung <shiningdubhe@gmail.com>
2024-04-25 17:35:40 +0900

    2024.04.25 16: 30 Stack에서 MutableList로 변경

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -7,8 +7,7 @@ class Calculator(inputs: String) {
 
 
     fun calc(): Double {
-        num.forEach{println(it)}
-        sign.forEach{println(it)}
+
         firstCalc()
         val result = lastCalc()
 
@@ -26,6 +25,8 @@ class Calculator(inputs: String) {
                 num.removeAt(i + 1)
                 sign.removeAt(i)
 
+                num.forEach { println(it) }
+
             } else if (sign[i].equals("/")) {
                 num[i] = div.operate(num[i], num[i + 1])
                 num.removeAt(i + 1)
@@ -39,17 +40,14 @@ class Calculator(inputs: String) {
     private fun lastCalc(): Double {
         val add = AddOperation()
         val sub = SubstractOperation()
-        //남은 연산
+
         var result = num[0]
 
         for (j in sign.indices) {
             if (sign[j].equals("+")) {
                 result = add.operate(result, num[j + 1])
-                println("${result}++++++++++++++++${j}")
             } else if (sign[j].equals("-")) {
-                println(result)
                 result = sub.operate(result, num[j + 1])
-                println("${result}---------------${j}")
             }
         }
         return result
--- src/main/kotlin/Changs.kt
+++ src/main/kotlin/Changs.kt
@@ -1,18 +1,12 @@
-import java.util.Stack
-
 class Changs {
 
-    fun num(inputs: String) : Stack<Double> {
-        val num = inputs.split("\\D".toRegex()).map { it.toDoubleOrNull() }.filterNotNull().toList()
-        val numbers = Stack<Double>()
-        numbers.addAll(num)
-        return numbers
+    fun num(inputs: String): MutableList<Double> {
+        val num = inputs.split("\\D".toRegex()).map { it.toDoubleOrNull() }.filterNotNull().toMutableList()
+        return num
     }
 
-    fun sign(inputs: String) : Stack<String> {
-        val sign =("\\+|\\-|\\/|\\*".toRegex()).findAll(inputs).map { it.value }.toList()
-        val signs = Stack<String>()
-        signs.addAll(sign)
-        return signs
+    fun sign(inputs: String): MutableList<String> {
+        val sign = ("\\+|\\-|\\/|\\*".toRegex()).findAll(inputs).map { it.value }.toMutableList()
+        return sign
     }
 }
