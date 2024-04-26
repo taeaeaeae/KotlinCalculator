e0ad1990b9a0778d36937ee0e8cf55f1fad1fdd8
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-04-26 11:12:20 +0900

    Committing changes before pull with rebase

--- src/main/kotlin/AbstractOperation.kt
+++ src/main/kotlin/AbstractOperation.kt
@@ -1,3 +1,6 @@
 
 abstract class AbstractOperation {
+    fun operate(num1: Double, num2: Double) : Double {
+        return num1+num2
+    }
 }
--- src/main/kotlin/AddOperation.kt
+++ src/main/kotlin/AddOperation.kt
@@ -1,6 +1,7 @@
 
 
 class AddOperation {
+    //override
     fun operate(num1: Double, num2: Double): Double {
         return num1+num2
     }
